369c990a7f93dcdca27bde301269f4a9ddd4a355
Omar Biber <omarbiber.67@gmail.com>
2024-09-24 10:11:04 +0200

    API constructeurs et maquette page inscription

--- espace-membres/inscription.php
+++ espace-membres/inscription.php
@@ -3,18 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/constructeurautomobile/style/espace-membres.css">
+    <link rel="stylesheet" href="../style/espace-membres.css">
     <title>CorsaAutomobile / Inscription</title>
 </head>
 <body>
 
 
 <!-- BARRE DE NAVIGATION -->
-
-<?php include '/components/navbar.html'; ?>
+<header>
+<?php include '../components/navbar-inscription.html'; ?>
 
 
 <!-- HEADER -->
+
+<main>
+    <div class="form-inscriptiion-container">
+        <form action="../components/navbar-loged.php">
+            <div class="box-1">
+            <label for="pseudo">Pseudo : </label>
+            <input type="text" id="pseudo">
+            </div>
+            
+            <div class="box-2">
+            <label for="email">Email : </label>
+            <input type="text" id="email">
+            </div>
+
+            <div class="box-3">
+            <label for="mdp">Mot de passe : </label>
+            <input type="text" id="mdp">
+            </div>
+
+            <input placeholder="envoyer" type="submit" id="submit" name="button" >
+        </form>
+    </div>
+</main>
+
+</header>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php 
+$apiUrl = "https://filrouge.uha4point0.fr/V2/car/constructeurs";
+$reponse = file_get_contents($apiUrl);
+$constructeurs = json_decode($reponse, true);
+
+$constructeursFiltre = array_filter($constructeurs, function($constructeur) {
+    return in_array($constructeur["id"], ['1', '2', '5']);
+});
+
+$logoscostructeurs = [
+    'Renault' => 'img/1-renaultt.png',
+    'BMW'=> 'img/2-bmw.png',
+    'Fiat'=> 'https://logos-marques.com/wp-content/uploads/2021/02/Fiat-Logo-2006.png'
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -38,25 +55,20 @@
         </div>
 
         <div class="container-cartes">
-            <div class="api-constructeur">
+
+            <?php foreach ($constructeursFiltre as $constructeur) : ?>
+
             <a class="voiture-lien" href="voiture-1.php" name="voiture" type="text">
-                <img src="img/1-renaultt.png" alt="Logo de Renault">
-                <h1 class="nom">Renault</h1>
-                <h2 class="creation">1899</h2>
-                <h2 class="fondateur">Fernand et Marcel Renault</h2>
-                <ul class="usines">
-                    <li>Usines :</li>
-                    <li>Palencia</li>
-                    <li>Douai</li>
-                    <li>Bursa</li>
-                    <li>France</li>
-                    <li >Sandouville</li>
-                    <li>Dieppe</li>
-                </ul>
-                <h2 class="pays">Pays : France</h2>
+            <div class="api-constructeur">
+                <img src="<?php echo $logoscostructeurs[$constructeur['nom']]; ?>" alt="Logo de Renault">
+                <h1 class="nom"><?php echo htmlspecialchars($constructeur['nom'], ENT_QUOTES, 'UTF-8'); ?></h1>
+                <h2 class="creation">Année de création : <?php echo htmlspecialchars($constructeur['creation'], ENT_QUOTES, 'UTF-8'); ?></h2>
+                <h2 class="fondateur">Fondataur(s) : <?php echo htmlspecialchars($constructeur['fondateur'], ENT_QUOTES, 'UTF-8'); ?></h2>
+                <h2 class="pays">Pays : <?php echo htmlspecialchars($constructeur['pays'], ENT_QUOTES, 'UTF-8'); ?></h2>
                 </a>
             </div>
-            
+           
+            <!--
             <div class="api-constructeur">
             <a class="voiture-lien" href="voiture-2.php" name="voiture" type="text" >
                 <img src="https://logos-marques.com/wp-content/uploads/2021/02/Fiat-Logo-2006.png" alt="Logo de Fiat">
@@ -94,7 +106,8 @@
                 <h2 class="pays">Pays : Allemagne</h2>
                 </a>
             </div>
-            
+            -->
+            <?php endforeach; ?>
         </div>
     </section>
 
